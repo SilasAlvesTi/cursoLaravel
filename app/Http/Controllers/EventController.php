b9f27956ac16c209ae80ed60b55e840d6a85a6a8
SilasAlvesTi <silas.ifce.ti@gmail.com>
2021-07-09 16:45:44 -0300

    feat: allow image in event create

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -20,12 +20,29 @@ class EventController extends Controller
 
     public function store(Request $request) {
         $event = new Event;
+        
+        $imagemDefault = 'img/banner.jpg';
 
         $event->title = $request->title;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
 
+        // image upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+
+        }
+        
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
