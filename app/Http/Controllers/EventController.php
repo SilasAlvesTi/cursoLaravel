acb444c02bf8c60baff51b76af2c726ac9e1cb01
SilasAlvesTi <silas.ifce.ti@gmail.com>
2021-07-15 15:39:46 -0300

    feat: Event owner name added to event description

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -57,7 +58,7 @@ class EventController extends Controller
         
         $user = auth()->user();
         $event->user_id = $user->id;
-        
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
@@ -67,6 +68,8 @@ class EventController extends Controller
     {
         $event = Event::findOrFail($id);
 
-        return view('events/show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events/show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 }
